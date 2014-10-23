c49d60ca23fa3042728c45779441c31f9849f34d
mitchg45 <mitchellgrogg@gmail.com>
2014-10-23 11:42:03 -0400

    add all in dir

---
+++ Debug/tm4c1294ncpdt_startup_ccs.pp
@@ -0,0 +1,7 @@
+# FIXED
+
+tm4c1294ncpdt_startup_ccs.obj: ../tm4c1294ncpdt_startup_ccs.c
+tm4c1294ncpdt_startup_ccs.obj: /opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h
+
+../tm4c1294ncpdt_startup_ccs.c: 
+/opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h: 
